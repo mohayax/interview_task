62cd63ee3f5a2a153b6ddd5d0ebcffacdd36e69d
Muhammad Yakubu <mohayax02@gmail.com>
2025-09-24 19:17:51 +0100

    update: blog serlizer likes update

--- blog/serializers.py
+++ blog/serializers.py
@@ -12,6 +12,7 @@ class CommentSerializer(serializers.ModelSerializer):
 class BlogSerializer(serializers.ModelSerializer):
     comments = CommentSerializer(many=True, read_only=True)
     author = UserSerializer(read_only=True)
+    likes = UserSerializer(many=True, read_only=True)
     class Meta:
         model = Blog
         fields = '__all__'
